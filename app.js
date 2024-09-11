7ba8f3b37556899152c63dccb5e21215ae183b44
Enis ÖZGÜR <zgr.enis@gmail.com>
2024-09-11 11:17:05 +0300

    comment lines

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const {engine}= require('express-handlebars') //seperate
 const expressSession=require('express-session') //keep info on host
 const fileUpload=require('express-fileupload') //image
 const dotenv = require('dotenv') //keep secret info (URL)
-const path = require('path')    //import path module
-const dbs= require(path.join(__dirname,'dbs.js'))
+const path = require('path')    //import path module                    /*require is pulling from module which include variable */
+const dbs= require(path.join(__dirname,'dbs.js'))      // "__dirname" is referance folder name for dbs.js
 
 //db connect
 dbs()
--- dbs.js
+++ dbs.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')    //keep secret
 dotenv.config()
 
 const conn = ()=>{
-    mongoose.connect(process.env.DB_URL,{
+    mongoose.connect(process.env.DB_URL,{   //process env, code reach the dburl in enviroments of files
         dbName:'myBlog'
     }).then(()=>{
         console.log('DB connected')
