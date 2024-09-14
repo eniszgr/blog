67b4f0dacfbfdedce5423bc75d2dfbe841bf35fc
Enis ÖZGÜR <zgr.enis@gmail.com>
2024-09-14 18:26:07 +0300

    server running

--- app.js
+++ app.js
@@ -9,3 +9,31 @@ const dbs= require(path.join(__dirname,'dbs.js'))      // "__dirname" is referan
 //db connect
 dbs()
 
+
+// initial settings
+dotenv.config()
+const app= express()
+
+
+// variables
+const time = 1000*60*30;
+
+// template engine
+app.engine('handlebars', engine())
+app.set('view engine','handlebars')
+app.set('views',path.join(__dirname,'views'))
+
+// middleware
+app.use(express.json())
+app.use(fileUpload())
+app.use(expressSession({
+    secret:'myBlog',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{path:'/' , httpOnly:true , secure: false , maxAge:time}
+}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(5000 , ()=>{
+    console.log(`Server is running http://127.0.0.1:5000`)
+})
