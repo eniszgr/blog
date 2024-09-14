771978d173a495b9143c85af3c97fc8146640c7e
Enis ÖZGÜR <zgr.enis@gmail.com>
2024-09-14 21:05:14 +0300

    view panels were edited as partial view. get methods added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const fileUpload=require('express-fileupload') //image
 const dotenv = require('dotenv') //keep secret info (URL)
 const path = require('path')    //import path module                    /*require is pulling from module which include variable */
 const dbs= require(path.join(__dirname,'dbs.js'))      // "__dirname" is referance folder name for dbs.js
+const crypto = require('crypto')
+console.log(crypto.randomBytes(64).toString('hex'))
 
 //db connect
 dbs()
@@ -17,6 +19,9 @@ const app= express()
 
 // variables
 const time = 1000*60*30;
+const SECRET_VALUE= process.env.SECRET_VALUE || 'myBlog'
+const PORT = process.env.PORT || 5000
+const API_URL = process.env.API_URL || 'http://127.0.0.1:5000'
 
 // template engine
 app.engine('handlebars', engine())
@@ -27,13 +32,40 @@ app.set('views',path.join(__dirname,'views'))
 app.use(express.json())
 app.use(fileUpload())
 app.use(expressSession({
-    secret:'myBlog',
+    secret:SECRET_VALUE,
     resave:false,
     saveUninitialized:true,
     cookie:{path:'/' , httpOnly:true , secure: false , maxAge:time}
 }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(5000 , ()=>{
-    console.log(`Server is running http://127.0.0.1:5000`)
+
+app.get('/', (req,res)=>{
+    res.render('site/index',{
+        source:'home.jpg',
+        title:'My Blog',
+        description:`It's a Dream`
+    })
+})
+app.get('/about', (req,res)=>{
+    res.render('site/about',{
+        source:'about.jpg',
+        title:'Why U.S',
+        description:`We Are a Family`
+    })
+})
+app.get('/login', (req,res)=>{
+    res.render('site/login')
+})
+app.get('/register', (req,res)=>{
+    res.render('site/register')
+})
+app.get('/add', (req,res)=>{
+    res.render('site/add')
+})
+
+
+
+app.listen(PORT , ()=>{
+    console.log(`Server is running ${API_URL}`)
 })
