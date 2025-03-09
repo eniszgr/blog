8f0288afb4759847408bd1175ca879b9574c95a2
eniszgr <zgr.enis@gmail.com>
2025-03-10 00:46:28 +0300

    delete operation added

--- router/singlePage.js
+++ router/singlePage.js
@@ -1,14 +1,73 @@
+const e = require('express')
 const express = require('express')
 const router = express.Router()
+const {join} = require('path')
+const Content =require(join(__dirname,'..', 'model','contentModel.js'))
 
-router.get('/', async(req,res)=>{
+const fs = require('fs') //file system
+
+router.get('/:id', async(req,res)=>{
     try {
-        return res.render('site/single') 
+        const{ id } = req.params
+        if(id.length != 24) {
+            return res.redirect('/error')
+            
+        }
+            
+        const data = await Content.findById(id).exec()
+        console.log(data)
+        console.log(id)
+        return res.render('site/single',{
+            singleData:data.toJSON()
+        }) 
     } catch (error) {
         console.log(error)
         return res.redirect('/error')
     }
 })
+router.delete('/:id', async(req,res)=>{
+    try {
+        if(!res.locals.user){
+            return res.json({
+                case:false,
+                message:'unauthorized'
+            })
+        }
+        let {id} = req.params
+        const data = await Content.findById(id).exec()
+        let fileName = data.path
+        console.log(fileName)
+        let pathName = join(__dirname,'..','public',fileName)
+        console.log(pathName)
+        
+        Content.findByIdAndDelete(id).then(()=>{
+            fs.unlink(pathName,(error)=>{
+                if(error !== null){
+                    return res.json({
+                        case:false,
+                        message:'delete was unsuccessful'
+                    })
+                }
+                return res.json({
+                    case:true,
+                    message:'delete was successful'
+                })
+            })
+            
+        }).catch((error)=>{
+            console.log(error)
+            return res.json({
+                case:false,
+                message:'delete was unsuccessful'
+            })
+        })
+        
+        
+    } catch (error) {
+     console.log(error)
+     return res.redirect('/error') 
+    }
+}) 
 
 
 
